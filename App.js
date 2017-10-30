851662d2b40dd32095f5d89584ddcf246f3ff02a
Pankamol Srikaew <aim.ske09@gmail.com>
2017-10-31 06:43:15 +0700

    add stylesheet

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { AComponent } from './AComponent'
+import style from './Style'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Nothing</Text>
+      <View style={style.container}>
+        <Text style={style.green}>Nothing</Text>
+        <AComponent />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  green: {
+    color: '#00b900'
+  },
+  container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+  }
+})
